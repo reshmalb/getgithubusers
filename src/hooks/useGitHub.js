9389fecabcd721f784afac0d1e9c21c2fb1c3300
reshmalb <reshma.lb27@gmail.com>
2023-06-12 14:32:32 +0530

    custom hook implemented

--- src/hooks/useGitHub.js
+++ src/hooks/useGitHub.js
@@ -1,13 +1,30 @@
 import React,{useState,useEffect} from 'react'
 
 
-const useGitHub = (usernaem) => {
+const useGitHub = (username) => {
     const [loading,setLoading]=useState(false);
-  return (
-    <div>
-      
-    </div>
-  )
+    const [error,setError]=useState(null);
+    const [user,setuser]=useState(null)
+    useEffect(()=>{
+        //https://api.github.com/users/username
+        const fetchData= async()=>{
+            setLoading(true);
+            try{
+                 const response= await fetch(`https://api.github.com/users/${username}`)
+                 const responsedata=await response.json();
+                 setuser(responsedata)
+                 setLoading(false)
+
+            }catch(error){
+                setError(error)
+                setLoading(false)
+
+            }
+        }
+        fetchData();
+
+    },[username])
+  return {user,loading,error}
 }
 
 export default useGitHub
