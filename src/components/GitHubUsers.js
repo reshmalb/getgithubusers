1dc5f93a5ffefbebde2fa7d493d940ac8abdf24d
reshmalb <reshma.lb27@gmail.com>
2023-06-12 14:24:26 +0530

    first commit

---
+++ src/components/GitHubUsers.js
@@ -0,0 +1,16 @@
+
+import React from 'react'
+
+const GitHubUsers = () => {
+  return (
+    <div>
+        <form>
+            <input type="text" placeholder='Enter username'></input>
+            <button>Search</button>
+        </form>
+      
+    </div>
+  )
+}
+
+export default GitHubUsers
---
+++ src/hooks/useGitHub.js
@@ -0,0 +1,13 @@
+import React,{useState,useEffect} from 'react'
+
+
+const useGitHub = (usernaem) => {
+    const [loading,setLoading]=useState(false);
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default useGitHub
